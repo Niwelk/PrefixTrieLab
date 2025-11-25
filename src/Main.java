c1f71e6a173919e1dcb21733015099797f73fcd3
Niwelk <ivan.chervenkov11@gmail.com>
2025-11-25 04:51:17 +0300

    Commit Update Main.java
    Add some commits

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 import Trie.*;
 
-// класс Main реализован просто, тк в условии лабораторной работы не было сказано про сложную и более удобную чтению реализацию
+// класс Main реализован просто (с помощью тестов), тк в условии лабораторной работы не было сказано про сложную и более удобную чтению реализацию
 // да, можно было бы сделать while(true), добавить scanner класс и пользователь бы сам определял работу программы, но в условия не было :D
 // было лишь сказано про то, чтобы реализовать класс префиксного дерева
 // поэтому в Main просто наглядно демонстрируется корректная работа конкретно префиксного дерева
@@ -15,6 +15,7 @@ public class Main {
 
         System.out.println("[start]\n");
 
+        // тест 1 на добавлению слов
         for (int i = 0; i < listWords.length; i++) {
             System.out.println("[+] добавлено слово: " + listWords[i]);
             trie.insert(listWords[i]);
@@ -24,20 +25,23 @@ public class Main {
 
         System.out.println("\n[1] Проверка наличия слова: ");
 
+        // тест 2 на наличие слов в дереве
         for (int i = 0; i < listWords.length; i++) {
             System.out.println(listWords[i] + " " + ">> " + trie.contains(listWords[i]));
         }
 
+        // тест 3 на проверку наличия определённых слов с конкретным префиксом
         System.out.print("\n[2] Проверка существования слов с префиксом: ");
 
-        String wordOfCheckPrefix1 = "ja";
+        String wordOfCheckPrefix1 = "ja"; // произвольный префикс
         System.out.println(wordOfCheckPrefix1);
 
         System.out.println("[result] " + trie.startsWith(wordOfCheckPrefix1) + "\n");
 
+        // тест 4 на вывод всех слов, которые соответствуют конкретному префиксу
         System.out.print("[3] Проверка получения всех слов по префиксу: ");
 
-        String wordOfCheckPrefix2 = "java";
+        String wordOfCheckPrefix2 = "java"; // произвольный префикс
         System.out.println(wordOfCheckPrefix2);
 
         LinkedList result = trie.getByPrefix(wordOfCheckPrefix2);
